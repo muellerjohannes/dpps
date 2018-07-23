6b7fba40795d1ca495917b4ed683d69e0d3127f2
Johannes Müller <JohannesCMu@aol.de>
2018-07-23 22:30:40 +0100

    Last changes for today.

--- learning-toy-example-bayes.R
+++ learning-toy-example-bayes.R
@@ -17,31 +17,16 @@ target <- function (theta) {
   return(x)
 }
 
-det(diag(rep(1, n)) + LFunction(c(-6, 2)))
-
-exp(-Loss(c(-6, 2)))
-
-target(c(-6, 2))
-target(c(-8, 2))
-target(c(-5, 0.5))
-
-# Run MH random walk.
+# Run MCMC and create a heatmap that also shows the MLE.
 time <- proc.time()
-x <- MCMC(target, c(-8, 2), MH=TRUE, 10^3, alpha=0.1)  # 3 for SliceSampling
+x <- MCMC(target, c(-8, 2), MH=FALSE, 10^3, alpha=3)  # 3 or 0.1
 proc.time() - time
-plot(t(x2), pch=16, col='black', cex=0.5, xlim=c(-9.5, -6.5), ylim=c(1.5, 2.8))
-k <- kde2d(x[1, ], x[2, ], n=200, lims = c(-11, -3, -1, 3))
-image(k, col=r, xlim=c(-10.5, -6.3), ylim=c(1.4, 2.8))
+plot(t(x), pch=16, col='black', cex=0.5, xlim=c(-9.5, -6.5), ylim=c(1.5, 2.8))
+k <- kde2d(x[1, ], x[2, ], n=1000, lims = c(-11, -3, -1, 3))
+image(k, col=r, xlim=c(-9.5, -6.3), ylim=c(1.4, 2.8))
 points(z[1], z[2], pch=4, lwd=3, col="green")
 lines(c(z[1], z[1]), c(1, 3), type="l", col="red", lwd="1.5")
 lines(c(-10, -6), c(z[2], z[2]), type="l", col="red", lwd="1.5")
 
 # Calculating the acceptence rate for the MH algorithm; around 25% is desired
 sum(x[, -1] != x[, 1:(length(x)/2 - 1)])/(length(x) - 1)
-
-time <- proc.time()
-SliceSampling(c(-8, 2), target, 4)
-proc.time() - time
-
-RandomInterval(c(-8, 2), 0.5, target, 3)
-
